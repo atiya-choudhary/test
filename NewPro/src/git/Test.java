6abc519ff9d6d9c73a7d67ce438d3754bf0f1332
atiya-choudhary <atiya123@eng.rizvi.edu.in>
2022-09-26 15:30:04 +0530

    hry

---
+++ NewPro/src/git/Test.java
@@ -0,0 +1,11 @@
+package git;
+
+public class Test {
+	public static void main(String args[]) {
+		System.out.println("Hello");
+		System.out.println("Atiyaaa");
+		System.out.println("Neha ko chup karo.com");
+		System.out.println("Sadiq ko bhagao .com");
+	}
+
+}
