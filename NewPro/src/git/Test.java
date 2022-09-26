dba57d0dcca8b84edea5772b8fe69900d454d8b9
atiya-choudhary <atiya123@eng.rizvi.edu.in>
2022-09-26 15:32:59 +0530

    ok

--- NewPro/src/git/Test.java
+++ NewPro/src/git/Test.java
@@ -6,6 +6,7 @@ public class Test {
 		System.out.println("Atiyaaa");
 		System.out.println("Neha ko chup karo.com");
 		System.out.println("Sadiq ko bhagao .com");
+		System.out.println("heyy");
 	}
 
 }
